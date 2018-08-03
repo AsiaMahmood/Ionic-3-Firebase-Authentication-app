0f7f3452f51f75cf89cc32417dea590e43999e24
Asia Iraqi <asya11asia@gmail.com>
2018-08-03 14:38:44 +0300

    Update credentials.ts

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyD6ZH4Sky2KDc6n54TJsyOE2ixGW9tzrUk",
-    authDomain: "ramadan-1b084.firebaseapp.com",
-    databaseURL: "https://ramadan-1b084.firebaseio.com",
-    projectId: "ramadan-1b084",
-    storageBucket: "ramadan-1b084.appspot.com",
-    messagingSenderId: "44460462348"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
